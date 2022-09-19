3e0cbc114c45d9aa97a37eb051546cad4661d67f
Canberk Pitirli <canberk_nurcan@hotmail.com>
2022-09-19 23:39:32 +0300

    Beam particles has been added

---
+++ Source/Shotia/HitScanWeapon.cpp
@@ -0,0 +1,54 @@
+#include "Shotia/HitScanWeapon.h"
+#include "Kismet/GameplayStatics.h"
+#include "particles/ParticleSystemComponent.h"
+#include "Engine/SkeletalMeshSocket.h"
+#include "CharacterController.h"
+
+void AHitScanWeapon::Fire(const FVector& HitPos)
+{
+	Super::Fire(HitPos);
+
+	const USkeletalMeshSocket* Muzzle = GetWeaponMesh()->GetSocketByName(FName("MuzzleFlash"));
+
+	APawn* OwnerPawn = Cast<APawn>(GetOwner());
+	if (Owner == nullptr) return;
+	AController* InstigatorController = OwnerPawn->GetController();
+
+	if (Muzzle)
+	{
+		FTransform SocketTransform = Muzzle->GetSocketTransform(GetWeaponMesh());
+		FVector Start = SocketTransform.GetLocation();
+		FVector End = Start + (HitPos - Start) * 1.25;
+
+		FHitResult hit;
+
+		FVector BeamEnd = End;
+
+		UWorld* world = GetWorld();
+		if (world)
+		{
+			world->LineTraceSingleByChannel(hit, Start, End, ECollisionChannel::ECC_Pawn);
+
+			if (hit.bBlockingHit)
+			{
+				BeamEnd = hit.ImpactPoint;
+				ACharacterController* player = Cast<ACharacterController>(hit.GetActor());
+				if (player && InstigatorController)
+				{
+					UGameplayStatics::ApplyDamage(player, Damage, InstigatorController, this, UDamageType::StaticClass());
+				}
+
+				if (ImpactParticles)
+					UGameplayStatics::SpawnEmitterAtLocation(world, ImpactParticles, hit.ImpactPoint, hit.ImpactNormal.Rotation());
+			}
+		}
+		if (BeamParticles)
+		{
+			UParticleSystemComponent* Beam = UGameplayStatics::SpawnEmitterAtLocation(world, BeamParticles, SocketTransform);
+			if (Beam)
+			{
+				Beam->SetVectorParameter(FName("Target"),BeamEnd);
+			}
+		}
+	}
+}
